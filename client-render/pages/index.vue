11891499a4d4e45797a0e44f1b2e88f3209c07b1
Steven Cheng <steven.cheng@teksbotics.com>
2019-08-14 15:30:09 +0800

    add page generate

---
+++ client-render/pages/index.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+section.section
+  .columns.is-mobile
+    card(title='Free' icon='github-circle') Open source on
+      a(href='https://github.com/buefy/buefy')  GitHub
+    card(title='Responsive' icon='cellphone-link')
+      b.has-text-grey
+        | Every
+      |  component is responsive
+    card(title='Modern' icon='alert-decagram')
+      | Built with
+      a(href='https://vuejs.org/')
+        | Vue.js
+      |  and
+      a(href='http://bulma.io/')
+        | Bulma
+    card(title='Lightweight' icon='arrange-bring-to-front')
+      | No other internal dependency
+
+</template>
+
+<script>
+import Card from '~/components/Card'
+export default {
+  name: 'HomePage',
+  components: { Card },
+  methods: {
+    hello () {
+      return 'hello'
+    }
+  }
+}
+</script>
